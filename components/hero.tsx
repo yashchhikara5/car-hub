ae975bfd0b643479e93a0a415c5caab90783b4c9
yashchhikara5 <yashchhikara5@gmail.com>
2023-09-12 18:49:36 +0530

    layout, Navbar and Footer added

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,27 +19,27 @@ const Hero = () => {
           Streamline your car rental experience with our effortless booking experience
         </p>
 
-        <CustomButton 
+        <CustomButton
           title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
       </div>
 
-      <div className="hero__mage-container">
+      <div className="hero__image-container">
         <div className="hero__image">
-          <Image 
+          <Image
             src="/hero.png"
             alt="Car Image"
             fill
             className="object-contain"
           />
-
-          <div className="hero__image-overlay" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
 }
- 
+
 export default Hero;
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from "./hero";
 import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export  {
   Hero,
-  CustomButton
+  CustomButton,
+  Navbar,
+  Footer
 }
