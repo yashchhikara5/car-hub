3da91b1021df80a1f01ec02455db170f5158ec68
yashchhikara5 <yashchhikara5@gmail.com>
2023-09-15 21:49:17 +0530

    Added server rendering for custom filters

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
 
 export default async function Home({ searchParams }) {
   const allCars = await fetchCars({
@@ -32,8 +33,8 @@ export default async function Home({ searchParams }) {
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,11 @@
 import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars( filters: FilterProps ) {
+export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
   const headers = {
     'X-RapidAPI-Key': 'a77b326e39msh4ddc6bb6354a244p11cef8jsn2d3033563dcf',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
   const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
@@ -45,5 +45,15 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append('modelYear', `${year}`);
   url.searchParams.append('angle', `${angle}`);
 
-  return `${url}` ;
+  return `${url}`;
+}
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search)
+
+  searchParams.set(type, value)
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+  return newPathname;
 }
